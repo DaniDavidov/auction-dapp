8d71fb9649e57a414a2340a5c69a48310c149c65
Daniel Davidov <danidavidov13.dd@gmail.com>
2024-03-25 11:48:55 +0200

    small fix -> Removed i_deedRepository state variable and now it's passed as an address paramenter to each function that interacts with DeedRepository

--- src/AuctionRepository.sol
+++ src/AuctionRepository.sol
@@ -15,15 +15,15 @@ contract AuctionRepository is ReentrancyGuard {
     error AuctionRepository__OnlyOwnerCanCancelAuction();
     error AuctionRepository__DeedOperatorMustBeAuctionContract();
 
-    DeedRepository private immutable i_deedRepository;
 
     Auction[] private auctions;
 
     mapping (address owner => uint256[] auctions) private ownerAuctions;
     mapping (uint256 auctionId => Bid[] bids) private auctionBids;
 
-    modifier contractIsDeedOperator(uint256 _tokenId) {
-        address operator = i_deedRepository.getApproved(_tokenId);
+    modifier contractIsDeedOperator(uint256 _tokenId, address _deedRepoAddress) {
+        DeedRepository memory deedRepository = DeedRepository(_deedRepoAddress);
+        address operator = deedRepository.getApproved(_tokenId);
         if (operator != address(this)) {
             revert AuctionRepository__DeedOperatorMustBeAuctionContract();
         }
@@ -48,10 +48,6 @@ contract AuctionRepository is ReentrancyGuard {
     event BidSuccess(address indexed bidder, uint256 indexed auctionId, uint256 bidAmount);
     event AuctionCancelled(uint256 indexed actionId);
     event AuctionFinalized(address indexed sender, uint256 indexed autionId);
-
-    constructor(address _deedRepoAddress) {
-        i_deedRepository = DeedRepository(_deedRepoAddress);
-    }
 
     // block direct payments to the contract
     receive() external payable {
@@ -127,12 +123,13 @@ contract AuctionRepository is ReentrancyGuard {
     /**
      * Implements CEI
      * @param _auctionId ID number of the auction
+     * @param _deedRepoAddress address of the DeedRepository contract
      * @dev Everyone can call this function so the ether and the deed ownership are not potentially locked
      * @dev The auction can be finalized if both conditions are respected:
      *      - The auction has ended
      *      - There is at least one bid for the auction
      */
-    function finalizeAuction(uint256 _auctionId) external nonReentrant {
+    function finalizeAuction(uint256 _auctionId, address _deedRepoAddress) external nonReentrant {
         Auction memory auction = getAuctionById(_auctionId);
         Bid[] memory bids = auctionBids[_auctionId];
 
@@ -155,7 +152,8 @@ contract AuctionRepository is ReentrancyGuard {
             }
 
             // return the deed ownership to the owner of the auction
-            i_deedRepository.safeTransferFrom(address(this), auction.owner, auction.deedId);
+            DeedRepository memory deedRepository = DeedRepository(_deedRepoAddress);
+            deedRepository.safeTransferFrom(address(this), auction.owner, auction.deedId);
             auctions[_auctionId].finalized = true;
             emit AuctionFinalized(msg.sender, _auctionId);
         }
@@ -164,12 +162,14 @@ contract AuctionRepository is ReentrancyGuard {
     /**
     * @dev Creates an auction with the given informatin
     * @param _deedId uint256 of the deed registered in DeedRepository
+    * @param _deedRepoAddress address of the DeedRepository contract
     * @param _auctionTitle string containing auction title
     * @param _startPrice uint256 starting price of the auction
     * @param _blockDeadline uint is the timestamp in which the auction expires
     */
-    function createAuction(uint256 _deedId, string memory _auctionTitle, uint256 _startPrice, uint _blockDeadline) external contractIsDeedOperator(_deedId) {
-        address owner = i_deedRepository.ownerOf(_deedId);
+    function createAuction(uint256 _deedId, address _deedRepoAddress, string memory _auctionTitle, uint256 _startPrice, uint _blockDeadline) external contractIsDeedOperator(_deedId) {
+        DeedRepository memory deedRepository = DeedRepository(_deedRepoAddress);
+        address owner = deedRepository.ownerOf(_deedId);
         uint256 auctionId = getAuctionsCount() + 1;
         Auction memory auction;
         auction.name = _auctionTitle;
@@ -188,9 +188,10 @@ contract AuctionRepository is ReentrancyGuard {
     /**
      * Implements CEI
      * @param _auctionId ID number of an auction
+     * @param _deedRepoAddress address of the DeedRepository contract
      * @dev The owner of the auction cancels the auction then he gets back his deed and the bidder gets back his ether 
      */
-    function cancelAuction(uint256 _auctionId) public nonReentrant {
+    function cancelAuction(uint256 _auctionId, address _deedRepoAddress) public nonReentrant {
         Auction memory auction = getAuctionById(_auctionId);
         if (auction.owner != msg.sender) {
             revert AuctionRepository__OnlyOwnerCanCancelAuction();
@@ -202,7 +203,8 @@ contract AuctionRepository is ReentrancyGuard {
 
         auctions[_auctionId].finalized = true;
 
-        i_deedRepository.safeTransferFrom(address(this), auction.owner, auction.deedId);
+        DeedRepository memory deedRepository = DeedRepository(_deedRepoAddress);
+        deedRepository.safeTransferFrom(address(this), auction.owner, auction.deedId);
 
         Bid[] memory bids = auctionBids[_auctionId];
         if (bids.length > 0) {
